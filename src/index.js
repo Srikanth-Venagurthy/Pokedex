951a61187e8bbc1363c7d31cb86bf3cd35bbbc2a
Srikanth-Venagurthy <venagurthy.srikanth@gmail.com>
2023-02-23 17:27:07 +0530

    reverted changes

--- src/index.js
+++ src/index.js
@@ -6,22 +6,22 @@ import * as serviceWorkerRegistration from './serviceWorkerRegistration';
 import reportWebVitals from './reportWebVitals';
 
 
-if (
-  navigator.userAgent.includes("Android") &&
-  navigator.userAgent.includes("Chrome")
-) {
-  window.addEventListener("DOMContentLoaded", () => {
-    const selector =
-      "a:not([data-no-sw-precache]):not([target]):not([download]):not([href^='mailto:']):not([href^='tel:'])";
-    const links = Array.from(document.querySelectorAll(selector));
-    links.forEach((link) => {
-      link.addEventListener("click", (e) => {
-        e.preventDefault();
-        window.location.href = link.href;
-      });
-    });
-  });
-}
+// if (
+//   navigator.userAgent.includes("Android") &&
+//   navigator.userAgent.includes("Chrome")
+// ) {
+//   window.addEventListener("DOMContentLoaded", () => {
+//     const selector =
+//       "a:not([data-no-sw-precache]):not([target]):not([download]):not([href^='mailto:']):not([href^='tel:'])";
+//     const links = Array.from(document.querySelectorAll(selector));
+//     links.forEach((link) => {
+//       link.addEventListener("click", (e) => {
+//         e.preventDefault();
+//         window.location.href = link.href;
+//       });
+//     });
+//   });
+// }
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
